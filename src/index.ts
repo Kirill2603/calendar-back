e045ff606b1275faee80ced50605a07600a1034f
kirill2603 <k.radobolsky@gmail.com>
2022-07-16 18:33:55 +0300

    add cors, base schema, routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express, { Express } from 'express'
 import mongoose from 'mongoose'
 import { router } from './models/routes/router'
+import cors from 'cors'
 
 const app: Express = express()
 const port = 3000
 const DB_URL = 'mongodb+srv://kirill:kirill@cluster0.9120nn6.mongodb.net/calendar?retryWrites=true&w=majority'
 
 
+app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api', router)
 
