5856b16ec6ad96f8cbf5a20ffc2fd8ecedd1b3c6
kirill2603 <k.radobolsky@gmail.com>
2023-01-12 17:58:36 +0300

    fix event model

--- src/models/eventModel.ts
+++ src/models/eventModel.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose'
 
 const EventSchema = new mongoose.Schema({
   title: { type: String, required: [true, 'Please enter event title'], minLength: 3, maxLength: 100 },
-  description: { type: String, required: false, minLength: 3, maxLength: 100, default: '' },
+  description: { type: String, required: false, maxLength: 100, default: '' },
   priority: { type: Number, min: 0, max: 3, default: 0 },
   color: { type: String, enum: ['red', 'green', 'blue', 'purple', 'orange', 'yellow'], default: 'green' },
   date: { type: Date, required: true },
